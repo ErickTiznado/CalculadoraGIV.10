050e677ff19074e55221ecd00521153ba1eec7ec
ERICK MAURICIO TIZNADO RODRIGUEZ <USIS038521@ugb.edu.sv>
2021-09-04 19:45:56 -0600

    Boton Igual

--- ProyectoDeFormularioGrupo1/Form1.Designer.cs
+++ ProyectoDeFormularioGrupo1/Form1.Designer.cs
@@ -234,7 +234,7 @@ namespace ProyectoDeFormularioGrupo1
             this.Div_Button.Name = "Div_Button";
             this.Div_Button.Size = new System.Drawing.Size(96, 36);
             this.Div_Button.TabIndex = 89;
-            this.Div_Button.Text = "÷";
+            this.Div_Button.Text = "/";
             this.Div_Button.UseVisualStyleBackColor = false;
             this.Div_Button.Click += new System.EventHandler(this.Div_Button_Click);
             // 
@@ -821,7 +821,7 @@ namespace ProyectoDeFormularioGrupo1
             this.txtpanel.Location = new System.Drawing.Point(330, 110);
             this.txtpanel.Name = "txtpanel";
             this.txtpanel.ReadOnly = true;
-            this.txtpanel.Size = new System.Drawing.Size(238, 39);
+            this.txtpanel.Size = new System.Drawing.Size(238, 33);
             this.txtpanel.TabIndex = 21;
             // 
             // btndividir
@@ -1033,7 +1033,7 @@ namespace ProyectoDeFormularioGrupo1
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(903, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(903, 26);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -1049,30 +1049,30 @@ namespace ProyectoDeFormularioGrupo1
             this.calculadoraCientificaToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.calculadoraCientificaToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.calculadoraCientificaToolStripMenuItem.Name = "calculadoraCientificaToolStripMenuItem";
-            this.calculadoraCientificaToolStripMenuItem.Size = new System.Drawing.Size(81, 26);
+            this.calculadoraCientificaToolStripMenuItem.Size = new System.Drawing.Size(70, 22);
             this.calculadoraCientificaToolStripMenuItem.Text = "Modos";
             // 
             // calculadoraCientificaToolStripMenuItem1
             // 
             this.calculadoraCientificaToolStripMenuItem1.Name = "calculadoraCientificaToolStripMenuItem1";
-            this.calculadoraCientificaToolStripMenuItem1.Size = new System.Drawing.Size(285, 6);
+            this.calculadoraCientificaToolStripMenuItem1.Size = new System.Drawing.Size(230, 6);
             // 
             // conversionToolStripMenuItem
             // 
             this.conversionToolStripMenuItem.Name = "conversionToolStripMenuItem";
-            this.conversionToolStripMenuItem.Size = new System.Drawing.Size(288, 26);
+            this.conversionToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
             this.conversionToolStripMenuItem.Text = "Conversiones";
             this.conversionToolStripMenuItem.Click += new System.EventHandler(this.conversionToolStripMenuItem_Click);
             // 
             // vToolStripMenuItem
             // 
             this.vToolStripMenuItem.Name = "vToolStripMenuItem";
-            this.vToolStripMenuItem.Size = new System.Drawing.Size(285, 6);
+            this.vToolStripMenuItem.Size = new System.Drawing.Size(230, 6);
             // 
             // calculadoraCientificaToolStripMenuItem2
             // 
             this.calculadoraCientificaToolStripMenuItem2.Name = "calculadoraCientificaToolStripMenuItem2";
-            this.calculadoraCientificaToolStripMenuItem2.Size = new System.Drawing.Size(288, 26);
+            this.calculadoraCientificaToolStripMenuItem2.Size = new System.Drawing.Size(233, 22);
             this.calculadoraCientificaToolStripMenuItem2.Text = "Calculadora Cientifica";
             this.calculadoraCientificaToolStripMenuItem2.Click += new System.EventHandler(this.calculadoraCientificaToolStripMenuItem2_Click);
             // 
@@ -1081,7 +1081,7 @@ namespace ProyectoDeFormularioGrupo1
             this.ayudaToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ayudaToolStripMenuItem.ForeColor = System.Drawing.Color.White;
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(80, 26);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(66, 22);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click_1);
             // 
@@ -1090,13 +1090,13 @@ namespace ProyectoDeFormularioGrupo1
             this.acercaDeToolStripMenuItem.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.acercaDeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.Control;
             this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
-            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(133, 26);
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
             this.acercaDeToolStripMenuItem.Text = "Acerca de...";
             this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(903, 495);
             this.Controls.Add(this.panel1);
